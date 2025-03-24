6198596348751fc576da78ad2ce2097b0248c0fd
jennapucciani <jpuccian@nd.edu>
2025-03-24 14:58:44 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,138 @@
 def on_forever():
-    pass
+    #basic.show_string("PYTHONDIRECTION")
+    #music.tone_playable(Note.C,10)
+    basic.show_leds("""
+    # # # . .
+    # . . # .
+    # # # . .
+    # . . . .
+    # . . . .
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # . . . #
+    . # . # .
+    . . # . .
+    . . # . .
+    . . # . .
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # . . . #
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # . . . #
+    # # . . #
+    # . # . #
+    # . . # #
+    # . . . #
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # .
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    # # # # #
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # # # . .
+    # . . # .
+    # # # . .
+    # . . # .
+    # . . # .
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # # # # .
+    # . . . .
+    # # # . .
+    # . . . .
+    # # # # .
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    . # # # .
+    # . . . .
+    # . . . .
+    # . . . .
+    . # # # .
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    # # # # #
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    """)
+    pause(1000)
+    basic.clear_screen()
+    basic.show_leds("""
+    # . . . #
+    # # . . #
+    # . # . #
+    # . . # #
+    # . . . #
+    """)
+    pause(1000)
 basic.forever(on_forever)
-#lala
--- main.ts
+++ main.ts
@@ -1,3 +1,138 @@
 basic.forever(function on_forever() {
-    
+    // basic.show_string("PYTHONDIRECTION")
+    // music.tone_playable(Note.C,10)
+    basic.showLeds(`
+    # # # . .
+    # . . # .
+    # # # . .
+    # . . . .
+    # . . . .
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # . . . #
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # . . . #
+    # # . . #
+    # . # . #
+    # . . # #
+    # . . . #
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # .
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    # # # # #
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # # # . .
+    # . . # .
+    # # # . .
+    # . . # .
+    # . . # .
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # # # # .
+    # . . . .
+    # # # . .
+    # . . . .
+    # # # # .
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    . # # # .
+    # . . . .
+    # . . . .
+    # . . . .
+    . # # # .
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    # # # # #
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
+    pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+    # . . . #
+    # # . . #
+    # . # . #
+    # . . # #
+    # . . . #
+    `)
+    pause(1000)
 })
